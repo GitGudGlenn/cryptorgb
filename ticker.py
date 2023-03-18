309f3d402b3e66bcbc6e7c4b9a46fd3540d27449
Glenn <Hulscher12@gmail.com>
2023-03-18 13:02:26 +0100

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -35,7 +35,7 @@ class Ticker(Frame):
 
     def get_symbols(self):
         """Get the symbols to include"""
-        symbols = os.environ.get('SYMBOLS', 'req, shib, bog')
+        symbols = os.environ.get('SYMBOLS', 'req, shiba, bog')
         if not symbols:
             return 'btc,eth'
         return symbols
